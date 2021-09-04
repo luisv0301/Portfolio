bcabf795036323d95044d9f3b4629644e447121c
Luis Vasquez <luisvbdr0301@gmail.com>
2021-09-04 16:55:29 -0400

    feat: hamburger menu and logo added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,43 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <h4 className="navbar__logo">Logo</h4>
-      <ul className="navbar__list">
+      <p className="navbar__logo">LV.</p>
+      <button
+        className="navbar__hamburmenu"
+        aria-label="menu"
+        aria-haspopup="true"
+        aria-expanded={isMenuOpen ? true : false}
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          role="img"
+          width="1em"
+          height="1em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 48 48"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M7.95 11.95h32" />
+            <path d="M7.95 23.95h32" />
+            <path d="M7.95 35.95h32" />
+          </g>
+        </svg>
+      </button>
+
+      <ul className={` navbar__list ${isMenuOpen ? "isVisible" : ""} `}>
         <li className="navbar__items">
           <a href="#skills" className="navbar__link">
             Skills
