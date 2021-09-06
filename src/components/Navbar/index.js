b22a4e0a6c431d34bc58b0f91550c8377629260c
Luis Vasquez <luisvbdr0301@gmail.com>
2021-09-06 15:12:26 -0400

    fix: avoid the navbar from expanding

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,56 +5,58 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="navbar">
-      <p className="navbar__logo">LV.</p>
-      <button
-        className="navbar__hamburmenu"
-        aria-label="menu"
-        aria-haspopup="true"
-        aria-expanded={isMenuOpen ? true : false}
-        onClick={() => setIsMenuOpen((prev) => !prev)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          width="1em"
-          height="1em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 48 48"
+    <div className="navbar__wrapper">
+      <nav className="navbar">
+        <p className="navbar__logo">LV.</p>
+        <button
+          className="navbar__hamburmenu"
+          aria-label="menu"
+          aria-haspopup="true"
+          aria-expanded={isMenuOpen ? true : false}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
         >
-          <g
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            width="1em"
+            height="1em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 48 48"
           >
-            <path d="M7.95 11.95h32" />
-            <path d="M7.95 23.95h32" />
-            <path d="M7.95 35.95h32" />
-          </g>
-        </svg>
-      </button>
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M7.95 11.95h32" />
+              <path d="M7.95 23.95h32" />
+              <path d="M7.95 35.95h32" />
+            </g>
+          </svg>
+        </button>
 
-      <ul className={` navbar__list ${isMenuOpen ? "isVisible" : ""} `}>
-        <li className="navbar__items">
-          <a href="#skills" className="navbar__link">
-            Skills
-          </a>
-        </li>
-        <li className="navbar__items">
-          <a href="#projects" className="navbar__link">
-            Projects
-          </a>
-        </li>
-        <li className="navbar__items">
-          <a href="#contact" className="navbar__link">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+        <ul className={` navbar__list ${isMenuOpen ? "isVisible" : ""} `}>
+          <li className="navbar__items">
+            <a href="#skills" className="navbar__link">
+              Skills
+            </a>
+          </li>
+          <li className="navbar__items">
+            <a href="#projects" className="navbar__link">
+              Projects
+            </a>
+          </li>
+          <li className="navbar__items">
+            <a href="#contact" className="navbar__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
